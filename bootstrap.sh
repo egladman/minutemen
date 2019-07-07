0ea6d72b9a2b65d13ba9ce0e1d363651a072fd94
Eli Gladman <8713153+egladman@users.noreply.github.com>
2019-07-07 16:21:38 -0400

    script out vanilla minecraft support

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # MC_* denotes Minecraft or Master Chief 
-MC_DOWNLOAD_URL="https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar" # Latest download link as of 7/06/2019
-MC_DOWNLOAD_SHA256SUM="942256f0bfec40f2331b1b0c55d7a683b86ee40e51fa500a2aa76cf1f1041b38"
-MC_VERSION="1.14.3"
-
 MC_INSTALL_DIR="/opt/minecraft"
 MC_MAX_HEAP_SIZE="896M" # Not some random number i pulled out of a hat: 1024-128
 MC_USER="minecraft" # For the love of god don't be an asshat and change to "root"
-MC_JAR_PATH="${MC_INSTALL_DIR}/server.${MC_VERSION}.jar"
 MC_EXECUTABLE_PATH="${MC_INSTALL_DIR}/start.sh"
 MC_SYSTEMD_SERVICE_NAME="minecraftd"
 MC_SYSTEMD_SERVICE_PATH="/etc/systemd/system/${MC_SYSTEMD_SERVICE_NAME}.service"
 
 # M_* denotes Minecraft Mod
-M_FORGE_DOWNLOAD_URL="https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.5.2837/forge-1.12.2-14.23.5.2837-installer.jar"
-M_FORGE_DOWNLOAD_SHA1="e4fd5f2ade6f4d6d3e18971fa18d8aade6ba1358"
+M_FORGE_DOWNLOAD_URL="https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.14.3-27.0.25/forge-1.14.3-27.0.25-installer.jar"
+M_FORGE_DOWNLOAD_SHA1SUM="7b96f250e52584086591e14472b96ec2648a1c9c"
+M_FORGE_INSTALLER_JAR="$(basename ${M_FORGE_DOWNLOAD_URL})"
+M_FORGE_INSTALLER_JAR_PATH="${MC_INSTALL_DIR}/M_FORGE_INSTALLER_JAR"
 
-MC_DOWNLOAD_ACTUAL_SHA256SUM=""
-M_FORGE_DOWNLOAD_ACTUAL_SHA1=""
-M_FORGE_INSTALLER_JAR_PATH=""
+# SYS_* denotes System
+SYS_JAVA_PATH="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
+
+
+M_FORGE_DOWNLOAD_ACTUAL_SHA1SUM=""
 M_FORGE_UNIVERSAL_JAR_PATH=""
+M_FORGE_UNIVERSAL_JAR=""
 SYS_TOTAL_MEMORY_KB=""
 SYS_TOTAL_MEMORY_MB=""
 
@@ -73,53 +73,50 @@ else
     _log "${MC_INSTALL_DIR} already exists. Proceeding with install."
 fi
 
-_log "Downloading minecraft jar..."
-wget "${MC_DOWNLOAD_URL}" -O "${MC_JAR_PATH}" || _die "Failed to fetch ${MC_DOWNLOAD_URL}"
-
-# Validate file download integrity
-MC_DOWNLOAD_ACTUAL_SHA256SUM="$(sha256sum ${MC_JAR_PATH} | cut -d' ' -f1)"
-if [ "${MC_DOWNLOAD_ACTUAL_SHA256SUM}" != "${MC_DOWNLOAD_SHA256SUM}" ]; then
-    _die "sha256sum doesn't match for ${MC_JAR_PATH}"
-fi
-
 _log "Downloading forge..."
 wget "${M_FORGE_DOWNLOAD_URL}" -P "${MC_INSTALL_DIR}" || _die "Failed to fetch ${M_FORGE_DOWNLOAD_URL}"
 
-# TODO: Validate Forge SHA1
+# Validate file download integrity
+M_FORGE_DOWNLOAD_ACTUAL_SHA1SUM="$(sha1sum ${M_FORGE_INSTALLER_JAR_PATH} | cut -d' ' -f1)"
+if [ "${M_FORGE_DOWNLOAD_ACTUAL_SHA1SUM}" != "${M_FORGE_DOWNLOAD_SHA1SUM}" ]; then
+    _die "sha1sum doesn't match for ${M_FORGE_INSTALLER_JAR_PATH}"
+fi
 
 # Install Ubuntu dependencies
 apt_dependencies=(
-    "openjdk-11-jdk"
-    "libsfml-dev"
+    "openjdk-8-jdk" # openjdk-11-jdk works fine with vanilla Minecraft, but not with Forge
 )
 command -v apt-get >/dev/null 2>&1 && sudo apt-get update -y && sudo apt-get install -y "${apt_dependencies[@]}"
+
+#Configure /usr/bin/java to point to openjdk-8 instead of openjdk-11
+update-alternatives --set java "${SYS_JAVA_PATH}" || _die "Failed to default java to openjdk-8"
 
 SYS_TOTAL_MEMORY_KB="$(grep MemTotal /proc/meminfo | awk '{print $2}')"
 SYS_TOTAL_MEMORY_MB="$(( $SYS_TOTAL_MEMORY_KB / 1024 ))"
 MC_MAX_HEAP_SIZE="$(( $SYS_TOTAL_MEMORY_MB - 128 ))M" # Leave 128MB memory for the system to run properly
 
-cat << EOF > "${MC_EXECUTABLE_PATH}"
-#!/bin/bash
-java -Xmx${MC_MAX_HEAP_SIZE} -jar ${MC_JAR_PATH}
-EOF
-
 chown -R "${MC_USER}":"${MC_USER}" "${MC_INSTALL_DIR}"
 chmod +x "${MC_EXECUTABLE_PATH}" || _die "Failed to perform chmod on ${MC_EXECUTABLE_PATH}"
+
+su - "${MC_USER}" -c "cd ${MC_INSTALL_DIR}; java -jar ${M_FORGE_INSTALLER_JAR_PATH} --installServer" || {
+    _die "Failed to execute ${M_FORGE_INSTALLER_JAR_PATH}"
+}
+_success "${M_FORGE_INSTALLER_JAR completed!}"
+
+# the "cd" ensures we get just the basename 
+M_FORGE_UNIVERSAL_JAR="$(cd ${MC_INSTALL_DIR}; ls ${MC_INSTALL_DIR}/forge-*.jar | grep -v ${M_FORGE_INSTALLER_JAR})" #We will run into issues if multiple versions of forge are present
+M_FORGE_UNIVERSAL_JAR_PATH="${MC_INSTALL_DIR}/${M_FORGE_UNIVERSAL_JAR}"
+
+# Create the wrapper script that systemd invokes
+cat << EOF > "${MC_EXECUTABLE_PATH}"
+#!/bin/bash
+java -Xmx${MC_MAX_HEAP_SIZE} -jar ${M_FORGE_UNIVERSAL_JAR_PATH}
+EOF
 
 su - "${MC_USER}" -c "cd ${MC_INSTALL_DIR}; /bin/bash ${MC_EXECUTABLE_PATH}" && {
     # When executed for the first time, the process will exit. We need to accept the EULA
     _log "Accepting end user license agreement"
     sed -i -e 's/false/true/' "${MC_INSTALL_DIR}/eula.txt" || _die "Failed to modify ${MC_INSTALL_DIR}/eula.txt"
-}
-
-M_FORGE_INSTALLER_JAR_PATH="$(ls ${MC_INSTALL_DIR}/forge-*installer.jar)"
-su - "${MC_USER}" -c "cd ${MC_INSTALL_DIR}; java -jar ${M_FORGE_INSTALLER_JAR_PATH} --installServer" || {
-    _die "Failed to execute ${M_FORGE_INSTALLER_JAR_PATH}"
-}
-
-M_FORGE_UNIVERSAL_JAR_PATH="$(ls ${MC_INSTALL_DIR}/forge-*universal.jar)"
-su - "${MC_USER}" -c "cd ${MC_INSTALL_DIR}; java -jar ${M_FORGE_UNIVERSAL_JAR_PATH}" || {
-    _die "Failed to execute ${M_FORGE_UNIVERSAL_JAR_PATH}"
 }
 
 cat << EOF > "${MC_SYSTEMD_SERVICE_PATH}" || _die "Failed to create systemd service"
@@ -146,5 +143,5 @@ _log "Starting ${MC_SYSTEMD_SERVICE_NAME}.service. This can take awhile... Go gr
 systemctl start "${MC_SYSTEMD_SERVICE_NAME}" || _die "Failed to start ${MC_SYSTEMD_SERVICE_NAME} with systemd"
 
 ip_addresses="$(hostname -I)"
-_success "Server is now accessible from the following ip addresses: ${ip_addresses}"
+_success "Server is now running. Go crazy: ${ip_addresses}"
  
